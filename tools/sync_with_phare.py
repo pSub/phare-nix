749773ec2466c1eeca1cba402ae3e679ab148a1e
Pascal Wittmann <pascal.wittmann@red6-es.de>
2025-04-22 15:32:22 +0200

    Add doc-strings

--- tools/sync_with_phare.py
+++ tools/sync_with_phare.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python
 
+"""
+    Synchronizes the local configuration with the configuration on phare.io. The local configuration
+    always has precedence.
+"""
+
 import argparse
 import json
 import logging
@@ -21,6 +26,12 @@ PHARE_ENDPOINT = os.getenv('PHARE_ENDPOINT', "https://api.phare.io")
 
 
 def list_monitors():
+    """
+    Returns a json representation of all monitors on phare.io which are visible for the account
+    connected to the authentication token.
+
+    :return: a json representation of the monitors on phare.io
+    """
     r = requests.get(urllib.parse.urljoin(PHARE_ENDPOINT, "uptime/monitors"),
                      headers={"Authorization": "Bearer " + PHARE_TOKEN},
                      timeout=10)
@@ -28,6 +39,11 @@ def list_monitors():
 
 
 def create_monitor(json_object):
+    """
+    Creates a monitor according to `json_object` on phare.io.
+
+    :param json_object: a json representation of a monitor
+    """
     requests.post(urllib.parse.urljoin(PHARE_ENDPOINT, "uptime/monitors"),
                   data=json.dumps(json_object),
                   headers={"Authorization": "Bearer " + PHARE_TOKEN,
@@ -36,6 +52,12 @@ def create_monitor(json_object):
 
 
 def update_monitor(id_, json_object):
+    """
+    Updates the monitor corresponding to id `id_` on phare.io with data in `json_object`.
+
+    :param id_: id of a monitor
+    :param json_object: a json representation of a monitor
+    """
     requests.post(urllib.parse.urljoin(PHARE_ENDPOINT, "uptime/monitors/" + str(id_)),
                   data=json.dumps(json_object),
                   headers={"Authorization": "Bearer " + PHARE_TOKEN,
@@ -44,22 +66,43 @@ def update_monitor(id_, json_object):
 
 
 def pause_monitor(id_):
+    """
+    Pauses the monitor corresponding to id `id_` on phare.io.
+
+    :param id_: id of a monitor
+    """
     requests.post(urllib.parse.urljoin(PHARE_ENDPOINT, "uptime/monitors/" + str(id_) + "/pause"),
                   headers={"Authorization": "Bearer " + PHARE_TOKEN},
                   timeout=10)
 
 
 def resume_monitor(id_):
+    """
+    Resumes the monitor corresponding to id `id_` on phare.io.
+
+    :param id_: id of a monitor
+    """
     requests.post(urllib.parse.urljoin(PHARE_ENDPOINT, "uptime/monitors/" + str(id_) + "/resume"),
                   headers={"Authorization": "Bearer " + PHARE_TOKEN},
                   timeout=10)
 
 
 def camel_to_snake(camel_case_string):
+    """
+
+    :param camel_case_string: an arbitrary string
+    :return: the snake-case transformation of `camel_case_string`
+    """
     return reduce(lambda x, y: x + ('_' if y.isupper() else '') + y.lower(), camel_case_string)
 
 
 def as_snake(dct):
+    """
+    Transforms the keys in dictionary `dct` to snake-case.
+
+    :param dct: a dictionary
+    :return: the dictionary `dct` with snake-case keys
+    """
     new_dct = {}
     for key, value in dct.items():
         new_dct[camel_to_snake(key)] = value
@@ -67,6 +110,14 @@ def as_snake(dct):
 
 
 def monitor_diff(local_monitor, phare_monitor):
+    """
+    Compares the monitors `local_monitor` and `phare_monitor` while ignoring runtime attributes
+    and unused optional attributes.
+
+    :param local_monitor: a json representation of a monitor
+    :param phare_monitor: a json representation of a monitor
+    :return: the difference between the monitors
+    """
     ignored = ["root['id']", "root['response_time']", "root['updated_at']",
                "root['created_at']", "root['paused']", "root['status']"]
 
@@ -80,6 +131,10 @@ def monitor_diff(local_monitor, phare_monitor):
 
 
 def main():
+    """
+    Synchronizes the local configuration with the configuration on phare.io. The local configuration
+    always has precedence.
+    """
     parser = argparse.ArgumentParser()
     parser.add_argument('--monitorfile', type=argparse.FileType('r'))
     args = parser.parse_args()
