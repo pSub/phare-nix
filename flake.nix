3e46c6fd0148487ad541f44b906a9c39dc1dcd78
Pascal Wittmann <mail@pascal-wittmann.de>
2025-08-30 00:02:11 +0200

    Update nixpkgs version to 25.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "NixOS tests example";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
